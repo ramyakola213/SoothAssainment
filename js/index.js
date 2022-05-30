2d96ee2d865722f29abfdcca783e7343c93bd632
ramyakola213 <ramya.kola@zensar.com>
2022-05-30 19:09:31 +0530

    project add

--- js/index.js
+++ js/index.js
@@ -3,3 +3,14 @@ let start = img.src;
 let hover = img.getAttribute('data-hover'); 
 img.onmouseover = () => { img.src = hover; }
 img.onmouseout = () => { img.src = start; } 
+
+
+
+function respNav() {
+        var navbar = document.getElementById("hamburger");
+        if (navbar.style.display === "none") {
+          navbar.style.display = "block";
+        } else {
+          navbar.style.display = "none";
+        }
+      }
